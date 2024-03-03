8ef8fdbf6402ffee3e369129209a9cab86b0ef45
Muzakkir <muhammad-muzakkir.asad@ceative.co.uk>
2024-03-03 23:07:49 +0500

    Js Operators Done

---
+++ operators _ and_expressions/arithmatic_operators.js
@@ -0,0 +1,21 @@
+console.log("learning arithmetic operators");
+
+let a = 10;
+let b = 4;
+
+console.log("a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a * b = ", a*b);
+console.log("a / b = ", a/b);
+console.log("a % b = ", a%b);
+
+//advance operators
+console.log("a ** b = ", a**b);
+
+console.log("++a = ", ++a);
+console.log("a++ = ", a++);
+
+console.log("--a = ", --a);
+console.log("a-- = ", a--);
+
+console.log("a = ", a);
---
+++ operators _ and_expressions/assignment_operators.js
@@ -0,0 +1,16 @@
+console.log("assignment_operator");
+
+let assignment = 1;
+assignment  += 5 // same as assignment = assignment + 5
+console.log("value is now = ", assignment);
+
+assignment -=5; //same as assignment = assignment - 5
+console.log("value is now = ", assignment);
+
+
+assignment /=5 //same as assignment = assignment / 5
+console.log("value is now = ", assignment);
+
+assignment *=5 // same as assignment = assignment * 5
+console.log("value is now = ", assignment);
+
