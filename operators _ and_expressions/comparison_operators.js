39f19088d2637cd327b3a60b5e3e3601233011ae
Muzakkir <muhammad-muzakkir.asad@ceative.co.uk>
2024-03-04 22:20:45 +0500

    added comparison operators

---
+++ operators _ and_expressions/comparison_operators.js
@@ -0,0 +1,6 @@
+let comp1 = 6;
+let comp3 = 7;
+console.log("comp1 == comp1 is ", comp1 == comp3);
+console.log("comp1 != comp3 is ", comp1 != comp3);
+console.log("comp1 === comp3 is ", comp1=== comp3); // equal to value  and equal to type
+console.log("comp1 !== comp3 is ", comp1!== comp3); // not equal to value and not equal to type
