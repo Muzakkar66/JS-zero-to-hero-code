321f3d3c0cabbcc794aebcee7d36104930e64854
Muzakkir <muhammad-muzakkir.asad@ceative.co.uk>
2024-03-06 21:24:08 +0500

    added logical operators

---
+++ operators _ and_expressions/logical_operators.js
@@ -0,0 +1,13 @@
+//logical operators
+
+let x = 5;
+let y = 7;
+console.log(x >y && x == 5) // false true = false
+console.log(x < y && x== 7) //true false = false
+console.log(x < y || x==5)  //true true = true
+console.log(y< x) || x == 7 // false false = false
+
+//not operators wo hoty hain to !false is = true and !true is = false
+
+console.log(!true); // false
+console.log(!false); // true
