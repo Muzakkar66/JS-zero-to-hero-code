44a6459e99352926e69a350e22d43daa05d79970
Muzakkir <muhammad-muzakkir.asad@ceative.co.uk>
2024-03-07 20:16:12 +0500

    conditional statements if else

---
+++ conditional_expressions/if_else.js
@@ -0,0 +1,11 @@
+let a = prompt("enter age in number")
+a = Number.parseInt(a) // ype casting mean string to number conversion
+
+console.log( typeof a);
+
+
+if(a >= 28){
+    alert("bhai to shadi kr ly")
+} else{
+    alert("Saar kr abi")
+}
