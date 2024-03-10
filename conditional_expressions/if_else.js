2768a6563c20d9eeea19ce8d24842d7e629891cc
Muzakkir <muhammad-muzakkir.asad@ceative.co.uk>
2024-03-10 23:49:31 +0500

    added ternary operator which is based on condition true and false like

--- conditional_expressions/if_else.js
+++ conditional_expressions/if_else.js
@@ -1,11 +1,24 @@
-let a = prompt("enter age in number")
-a = Number.parseInt(a) // ype casting mean string to number conversion
+let a = prompt("enter age in number");
+a = Number.parseInt(a); // ype casting mean string to number conversion
 
-console.log( typeof a);
+console.log(typeof a);
 
-
-if(a >= 28){
-    alert("bhai to shadi kr ly")
-} else{
-    alert("Saar kr abi")
+if (a >= 28) {
+  alert("bhai to shadi kr ly");
+} else {
+  alert("Saar kr abi");
 }
+
+//ternary operator
+
+console.log("you can ", a < 18 ? "not drive" : " sssssd ffff");
+
+//another example of ternary operator
+
+let x = 5;
+let y;
+console.log(x);
+console.log(
+  "here is the example of ternary operator with condition",
+  x > 4 ? (y = "x") : "y ma pass nhi oi value"
+); //here if x ki value less hai 5 sy to true wali condition chaly ge and if value is greater ha to false wali
