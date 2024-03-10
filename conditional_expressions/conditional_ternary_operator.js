5c2a7106d485c52f2c6bb4b2d76e05508a59151e
Muzakkir <muhammad-muzakkir.asad@ceative.co.uk>
2024-03-11 00:04:16 +0500

    added ternary operator and switch condition

---
+++ conditional_expressions/conditional_ternary_operator.js
@@ -0,0 +1,13 @@
+//ternary operator
+let a = 23
+console.log("you can ", a < 18 ? "not drive" : " sssssd ffff");
+
+//another example of ternary operator
+
+let x = 5;
+let y;
+console.log(x);
+console.log(
+  "here is the example of ternary operator with condition",
+  x > 4 ? (y = "x") : "y ma pass nhi oi value"
+); //here if x ki value less hai 5 sy to true wali condition chaly ge and if value is greater ha to false wali
---
+++ conditional_expressions/switch_statement.js
@@ -0,0 +1,24 @@
+switch (new Date().getDay()) { //here is the condition where date function match with day value in case then execute it and pass the value to day variable
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+
+  console.log(day);
