296d913e1dca11b64e88a42a82c8f1d8b8348a5f
Muzakkir <muhammad-muzakkir.asad@ceative.co.uk>
2024-03-11 21:33:35 +0500

    added switch statement and if else problems set code

---
+++ practice_sets/operators_and_conditionals.js
@@ -0,0 +1,25 @@
+// //Chapter 2
+// //Problem 1
+let age = prompt("Enter the age of person.")
+
+if(age > 10 && age< 20){
+    console.log("The person fall in 10 to 20 age slat")
+}
+else{
+    console.log("Person not in 10 to 20 ");
+}
+
+
+//Problem 2
+let age1 = 14
+// num = Number.parseFloat(age1)
+switch (age1) {
+    case 12:
+        console.log("Person fall in 10 to 20 age category");
+        break
+    case 14:
+        console.log("Person fall in 10 to 20 age category");
+        break
+    default:
+        console.log("Your age is not special!");
+}
