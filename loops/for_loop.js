652b32a6266a49c00b47d010bbe62286e127b71a
Muzakkir <muhammad-muzakkir.asad@ceative.co.uk>
2024-03-19 21:37:33 +0500

    added for in loop

--- loops/for_loop.js
+++ loops/for_loop.js
@@ -1,18 +1,15 @@
 //Program to add first n natural number
-let sum = 0
+// let sum = 0
 
-let n = prompt("Enter the value of n: ")
+// let n = prompt("Enter the value of n: ")
 
-for(let i = 0; i< n; i++){
-    console.log('Here is the value ');
-
-    sum += (i + 1)
-
-}
-console.log("Sum of first" + n + "natural number is" + sum);
+// for(let i = 0; i< n; i++){
+//     console.log('Here is the value ');
+//     sum += (i + 1)
+// }
+// console.log("Sum of first" + n + "natural number is" + sum);
 
 // in loop
-
 const obje = {
     jack: 10,
     loop: 20,
@@ -22,4 +19,15 @@ const obje = {
 }
 for (const result in obje) {
    console.log(result);
+}
+
+//for in loop
+const object1 = {
+    jack: 10,
+    loop: 20,
+    donner: 30,
+    BC: 50
+}
+for (const iterator of 'jack') {
+    console.log(iterator);
 }
