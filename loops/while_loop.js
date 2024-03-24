fc08ca3d1b7825807b56d9c5c2284815b5cc8981
Muzakkir <muhammad-muzakkir.asad@ceative.co.uk>
2024-03-24 15:00:42 +0500

    added do  while loop

--- loops/while_loop.js
+++ loops/while_loop.js
@@ -1,7 +1,12 @@
- let n = prompt('Please enter the value of n: ')
+ let n = prompt('Please enter the value of n')
   n = Number.parseInt(n)
 let i = 0;
   while (i<n) {
     console.log(i);
     i++
   }
+ let v = 10
+  do {
+    console.log(`value is ${v}`);
+   v = v + 1;
+  } while (v<=20);
