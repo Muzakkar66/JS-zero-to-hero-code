c61052127c727453e90022c070ee0aca660a487d
Muzakkir <muhammad-muzakkir.asad@ceative.co.uk>
2024-04-04 22:00:02 +0500

    added string practise set with problem question

--- practice_sets/string.js
+++ practice_sets/string.js
@@ -6,3 +6,22 @@ const word = 'fox';
 console.log(sentence.includes(word))
 
 console.log(`The word ${word} ${sentence.includes(word) ? 'is' : 'is not '}in sentence`);
+
+
+//problem 3
+console.log(word.toUpperCase());
+console.log(word.toLowerCase());
+
+//problem 4 
+let st2 = 'please give rs 1000'
+let amount = Number.parseInt(st2.slice(15))
+console.log(amount);
+console.log(typeof amount);
+
+//problem 5
+
+let friend = 'haseeb'
+
+friend[3] = 't'
+
+console.log(friend); ///string canot change because exiting string is amutable 
